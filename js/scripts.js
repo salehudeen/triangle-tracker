9fe15da5c3725a6d967b1cf2c0200d0d832205f5
salehudeen <salehudeensaleh@gmail.com>
2019-04-27 07:53:08 +0200

    the backend works well at this stage

--- js/scripts.js
+++ js/scripts.js
@@ -1,26 +1,29 @@
-function track(){
-var l1=document.getElementById("l1")
-var l2=document.getElementById("l2")
-var l3=document.getElementById("l3")
+function track() {
+  var length1 = document.getElementById("l1").value;
+  var length2 = document.getElementById("l2").value;
+  var length3 = document.getElementById("l3").value;
 
-var a=[parseFloat(l1)];
-var b= [parseFloat(l2)];
-var c= [parseFloat(l3)];
+  var p = [parseFloat(length1)];
+  var q = [parseFloat(length2)];
+  var r = [parseFloat(length3)];
 
-if (a+b<= c||a+c<=b||b+c<=a) {
-    document.getElementById("result").innerHTML = "Hey There This isnt a triangle";
-  } else if (a === b & b === c & a > 0 & b > 0 & c > 0) {
+  var ab = p.concat(q);
+
+  var abc = ab.concat(r);
+
+  var lengthab = a + b;
+  var lengthca = a + c;
+  var lengthbc = b + c;
+
+  if (lengthab < c || lengthca < b || lengthbc < a) {
+    document.getElementById("result").innerHTML = "hey there this isnt a triangle!";
+  } else if (a === b && b === c && a > 0 && b > 0 && c > 0) {
     document.getElementById("result").innerHTML = "Equilateral Triangle.";
-  } else if (a === b & b !== c || a !== b & b === c & a & a > 0 & b > 0 & c > 0) {
+  } else if (a === b && b !== c || a !== b && b === c && a > 0 && b > 0 && c > 0) {
     document.getElementById("result").innerHTML = "Isosceles Triangle";
-  } else if (a !== b & b !== c & a >  0 & b> 0 & c > 0) {
+  } else if (a !== b && b !== c && (Number.isInteger(a) === true || Number.isInteger(b) === true || Number.isInteger(c) === true) && a > 0 && b > 0 && c > 0) {
     document.getElementById("result").innerHTML = "Scalene Triangle";;
   } else {
-    document.getElementById("result").innerHTML = "Invalid Measurements";
+    document.getElementById("result").innerHTML = "wrong values";
   };
-}
-
-function reset() {
-  document.getElementById("myform").reset();
-  document.getElementById("result").innerHTML = "Try other figures";
 }
